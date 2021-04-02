7c7b51aeba17303c0c12236fd43023b2b23d8691
thelonius182 <lcavdakker@gmail.com>
2021-04-02 20:45:25 +0200

    progress 2021-04-02

--- src/prep1.R
+++ src/prep1.R
@@ -16,13 +16,13 @@ ana_full <- tibble(
   lg_http_req = "c",
   lg_http_resp_sts = "d",
   lg_n_bytes = "e",
-  lg_some_url = "f",
+  lg_referrer = "f",
   lg_usr_agt = "g",
   lg_data_src = "h"
 )
 
 analyze_log <- function(logfile) {
-  # logfile <- "/home/lon/Documents/cz_streaming_logs/access.log.19"
+  # logfile <- "/home/lon/Documents/cz_streaming_logs/access.log.7"
   flog.info(paste0("log file: ", logfile), name = "cz_ana_log")
 
   # inlezen ----  
@@ -54,38 +54,24 @@ access_log$X1 <- iconv(access_log$X1, "UTF-8", "UTF-8", sub = '')
   )
   
   cz_log.2 <- cz_log.1 %>% 
-    mutate(lg_ip = fld1, cz_ts_raw = str_replace_all(fld4, "[\\[\\]]", '"'), lg_cz_ts = dmy_hms(cz_ts_raw), 
-           lg_http_req = str_replace_all(fld6, "[\"]", ""), lg_http_resp_sts = fld7, lg_n_bytes = fld8, 
-           lg_some_url = str_replace_all(fld9, "[\"]", ""), lg_usr_agt = str_replace_all(fld10, "[\"]", ""),
-           lg_some_url = str_replace_all(lg_some_url, "-", NA_character_),
+    mutate(lg_ip = fld1, 
+           cz_ts_raw = str_replace_all(fld4, "[\\[\\]]", '"'), 
+           lg_cz_ts = dmy_hms(cz_ts_raw), 
+           lg_http_req = str_replace_all(fld6, "[\"]", ""), 
+           lg_http_resp_sts = fld7, 
+           lg_n_bytes = fld8, 
+           lg_referrer = str_replace_all(fld9, "[\"]", ""), 
+           lg_referrer = str_replace_all(lg_referrer, "-", NA_character_),
+           lg_usr_agt = str_replace_all(fld10, "[\"]", ""),
            lg_usr_agt = str_replace_all(lg_usr_agt, "-", NA_character_),
-           lg_usr_agt = str_to_lower(lg_usr_agt)) %>% 
+           lg_usr_agt = str_to_lower(lg_usr_agt),
+           lg_session_length = as.integer(fld11)
+    ) %>% 
     select(starts_with("lg_")) %>%
     select(-lg_data_src, everything(), lg_data_src) %>% 
     arrange(lg_ip, lg_cz_ts)
 
-  # verwijder mislukte requests ----
-  # cz_log.3 <- cz_log.2 %>%
-  #   filter(!is.na(lg_n_bytes), lg_http_resp_sts == "200")
-  
-  # verwijder spiders/bots ----
-  # cz_log.4 <- cz_log.3 %>% 
-    # filter(str_detect(lg_usr_agt, pattern = "bot[\\s_ :,\\.\\;\\/\\-]")) %>% 
-    # filter(str_detect(lg_usr_agt, pattern = "curl")) 
-    # filter(str_detect(lg_usr_agt, pattern = "^mozilla/[45]\\.0$"))
-    # filter(str_detect(lg_usr_agt, pattern = "steeler"))  # firefox/[0-9]\\.|[0-1][0]\\.
-  # suppressMessages(
-  #   cz_log.7 <- cz_log.6 %>%
-  #     group_by(data_src, ip, title, cz_ts_ymd, cz_ts_h) %>%
-  #     summarise(t_start = min(cz_ts), t_stop = max(cz_ts)) %>%
-  #     mutate(cz_seconds = time_length(
-  #       interval(start = t_start, end = t_stop), "seconds"
-  #     )) %>%
-  #     filter(cz_seconds > 300L)
-  # )
-  
   rm(cz_log.1,
-     # cz_log.2,
      access_log)
   
   return(cz_log.2)
@@ -95,5 +81,7 @@ for (some_log in cz_log_files) {
   ana_single <- analyze_log(some_log)
   ana_full <- bind_rows(ana_full, ana_single)
 }
+
+# ana_full <- bind_rows(ana_full, cz_log.2)
 
 saveRDS(ana_full, file = "ana_full.RDS")
--- src/prep2.R
+++ src/prep2.R
@@ -5,41 +5,75 @@ library(readr)
 library(lubridate)
 library(fs)
 library(futile.logger)
+library(curlconverter)
+library(jsonlite)
+library(httr)
 
 fa <- flog.appender(appender.file("/home/lon/Documents/cz_ana.log"), "cz_ana_log")
 
 cz_log_files <- dir_ls(path = "/home/lon/Documents/cz_streaming_logs/")
 
-ana_full <- readRDS(file = "ana_full.RDS") 
+ana_full <- readRDS(file = "ana_full.RDS")
 
-feb_2021 <- interval(ymd("2021-02-01"), rollback(ymd("2021-03-01")))
+
+feb_2021 <- interval(ymd_hms("2021-02-01 00:00:00"), 
+                     rollback(ymd_hms("2021-03-01 23:59:59"), 
+                              preserve_hms = T), 
+                     tzone = "Europe/Amsterdam")
 
 # feb 2021 only
 ana_2021.02 <- ana_full %>%
-  filter(lg_cz_ts %within% feb_2021) %>% 
+  filter(lg_ip != "a" 
+         & lg_http_req != "c" 
+         & !is.na(lg_http_req) 
+         & lg_cz_ts %within% feb_2021
+         & lg_http_resp_sts == "200" 
+         & !is.na(lg_usr_agt)
+  ) %>%
   # split http-request
-  separate(lg_http_req, into = c("lg_http_cmd", "lg_cz_channel", "lg_http_protocol"), sep = " ") %>% 
-  # "OK"-responses only
-  filter(lg_http_resp_sts == "200") %>% 
+  separate(lg_http_req, 
+           into = c("lg_http_cmd", "lg_cz_channel", "lg_http_protocol"), 
+           sep = " ") %>%
   # drop redundant columns
-  select(-lg_http_cmd, -lg_http_protocol, -lg_some_url, -lg_http_resp_sts) %>% 
+  select(-lg_http_cmd, -lg_http_protocol, -lg_referrer, -lg_http_resp_sts) %>%
   # clean-up channel names & bytes
   mutate(lg_cz_channel = str_replace_all(lg_cz_channel, pattern = "/", replacement = ""),
          lg_n_bytes = as.numeric(lg_n_bytes))
 
-saveRDS(ana_2021.02, "ana_2021.02.RDS")
+ana_2021.02_A <- ana_2021.02 %>% 
+  mutate(lg_bitrate = round(lg_n_bytes * 8 / 1024 / lg_session_length, 0)) %>% 
 
-channels <- ana_2021.02 %>% select(lg_cz_channel) %>% distinct() %>% 
-  filter(!(str_detect(lg_cz_channel, pattern = "[.]|admin|test13") | str_length(str_trim(lg_cz_channel)) == 0))
+saveRDS(ana_2021.02_A, "ana_2021.02_A.RDS")
 
-# remove non-channel traffic
-cz_stats.01 <- ana_2021.02 %>% filter(lg_cz_channel %in% channels$lg_cz_channel)
+ana_2021.02_A <- readRDS(file = "ana_2021.02_A.RDS")
+
+channels <- ana_2021.02_A %>% select(lg_cz_channel) %>% distinct() %>% 
+  filter(!(str_detect(lg_cz_channel, pattern = "index|status|style|server|admin|test13|\\.(jpg|ico|png)") | str_length(str_trim(lg_cz_channel)) == 0))
+
+ana_2021.02_B <- ana_2021.02_A %>% 
+  # remove non-channel traffic
+  filter(lg_cz_channel %in% channels$lg_cz_channel) %>% 
+  mutate(lg_listened_secs = if_else(str_detect(lg_cz_channel, "\\blive\\b"), 
+                                    round(lg_n_bytes * 8 / 1024 / 256, 0), 
+                                    round(lg_n_bytes * 8 / 1024 / 128, 0))
+  )
+  
+cz_stats.01 <- ana_2021.02 %>% 
 
 # remove obvious bots
 cz_stats.02 <- cz_stats.01 %>% 
-  filter(!str_detect(lg_usr_agt, 
-                     pattern = "(bot|crawler|spider|checker|scanner|grabber|getter|java|python)[\\s_:,.;/-]?")) %>% 
-  filter(!str_detect(lg_usr_agt, pattern = "curl|wget"))
+  filter(!str_detect(lg_usr_agt,
+                     pattern = "(bot|crawler|spider|checker|scanner|grabber|getter)[\\s_:,.;/)-]?")
+                     # pattern = "(bot|spider)[\\s_:,.;/)-]")
+  )
+# 
+# cz_bots.1 <- cz_stats.01 %>% 
+#   filter(str_detect(lg_usr_agt, pattern = "^(curl|wget)"))
+# 
+# cz_bots.2 <- cz_stats.01 %>% 
+#   filter(str_detect(lg_usr_agt,
+#                     pattern = "(bot|crawler|spider|checker|scanner|grabber|getter)[\\s_:,.;/)-]")
+#          )
 
 # collect user-agents
 # stats_ua <-
@@ -48,14 +82,9 @@ cz_stats.02 <- cz_stats.01 %>%
 #             streamed_MB = round(sum(lg_n_bytes)/1024/1024, digits = 2))
 
 # reproduce csv-example
-# regex "live"  streams on 320 kbps = 40 kB/s. 30 sec session duration = 1200 kB = 1200 * 1024 bytes
-# regex !"live" streams on 160 kbps = 20 kB/s. 30 sec session duration =  600 kB =  600 * 1024 bytes
 cz_stats.03 <- cz_stats.02 %>% 
-  mutate(lg_date = date(lg_cz_ts),
-         lg_valid_session = if_else(str_detect(lg_cz_channel, pattern = "live"),
-                                    lg_n_bytes > 1228800,
-                                    lg_n_bytes > 614400)) %>% 
-  filter(lg_valid_session)
+  mutate(lg_date = date(lg_cz_ts)) %>% 
+  filter(lg_session_length >= 15L)
 
 cz_stats.04 <- cz_stats.03 %>% 
   group_by(lg_date) %>% 
@@ -64,3 +93,24 @@ cz_stats.04 <- cz_stats.03 %>%
 write_delim(cz_stats.04, delim = "\t", file = "cz_stats_verify.tsv")
 
 write_delim(channels, delim = "\t", file = "cz_stats_verify_channels.tsv")
+
+cz_curl <- "curl 'https://freegeoip.app/json/51.91.219.191' \
+  -H 'authority: freegeoip.app' \
+  -H 'dnt: 1' \
+  -H 'upgrade-insecure-requests: 1' \
+  -H 'user-agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0' \
+  -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
+  -H 'sec-fetch-site: none' \
+  -H 'sec-fetch-mode: navigate' \
+  -H 'sec-fetch-user: ?1' \
+  -H 'sec-fetch-dest: document' \
+  -H 'accept-language: nl-NL,nl;q=0.9,en-US;q=0.8,en;q=0.7' \
+  -H 'cookie: __cfduid=de21a991dc201214c6f5e3971f1d930621616846464' \
+  --compressed  "
+
+cz_straight <- straighten(cz_curl)
+
+cz_res <- make_req(cz_straight, add_clip = F)
+
+cz_geo <- toJSON(content(cz_res[[1]](), as="parsed"), auto_unbox = TRUE, pretty=TRUE)
+
