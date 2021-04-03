0dd68c2424ca8b87716b6507ff36fab666f1c190
thelonius182 <lcavdakker@gmail.com>
2021-04-03 12:40:33 +0200

    progress 2021-04-03

--- src/prep2.R
+++ src/prep2.R
@@ -21,14 +21,14 @@ feb_2021 <- interval(ymd_hms("2021-02-01 00:00:00"),
                               preserve_hms = T), 
                      tzone = "Europe/Amsterdam")
 
-# feb 2021 only
+# cleaning 1 ----
 ana_2021.02 <- ana_full %>%
-  filter(lg_ip != "a" 
-         & lg_http_req != "c" 
-         & !is.na(lg_http_req) 
-         & lg_cz_ts %within% feb_2021
-         & lg_http_resp_sts == "200" 
-         & !is.na(lg_usr_agt)
+  filter(lg_ip != "a" # remove dummy
+         & lg_http_req != "c" # remove dummy
+         & !is.na(lg_http_req) # valid http-requests only
+         & lg_cz_ts %within% feb_2021 # feb only
+         & lg_http_resp_sts == "200" # valid http-response
+         & !is.na(lg_usr_agt) # valid user-agents only
   ) %>%
   # split http-request
   separate(lg_http_req, 
@@ -40,17 +40,18 @@ ana_2021.02 <- ana_full %>%
   mutate(lg_cz_channel = str_replace_all(lg_cz_channel, pattern = "/", replacement = ""),
          lg_n_bytes = as.numeric(lg_n_bytes))
 
-ana_2021.02_A <- ana_2021.02 %>% 
-  mutate(lg_bitrate = round(lg_n_bytes * 8 / 1024 / lg_session_length, 0)) %>% 
+# ana_2021.02_A <- ana_2021.02 %>% 
+#   mutate(lg_bitrate = round(lg_n_bytes * 8 / 1024 / lg_session_length, 0)) 
 
-saveRDS(ana_2021.02_A, "ana_2021.02_A.RDS")
+saveRDS(ana_2021.02, "ana_2021.02.RDS")
 
-ana_2021.02_A <- readRDS(file = "ana_2021.02_A.RDS")
+# ana_2021.02 <- readRDS(file = "ana_2021.02.RDS")
 
-channels <- ana_2021.02_A %>% select(lg_cz_channel) %>% distinct() %>% 
+channels <- ana_2021.02 %>% select(lg_cz_channel) %>% distinct() %>% 
   filter(!(str_detect(lg_cz_channel, pattern = "index|status|style|server|admin|test13|\\.(jpg|ico|png)") | str_length(str_trim(lg_cz_channel)) == 0))
 
-ana_2021.02_B <- ana_2021.02_A %>% 
+# cleaning 2 ----
+cz_stats.01 <- ana_2021.02 %>% 
   # remove non-channel traffic
   filter(lg_cz_channel %in% channels$lg_cz_channel) %>% 
   mutate(lg_listened_secs = if_else(str_detect(lg_cz_channel, "\\blive\\b"), 
@@ -58,39 +59,36 @@ ana_2021.02_B <- ana_2021.02_A %>%
                                     round(lg_n_bytes * 8 / 1024 / 128, 0))
   )
   
-cz_stats.01 <- ana_2021.02 %>% 
-
 # remove obvious bots
 cz_stats.02 <- cz_stats.01 %>% 
   filter(!str_detect(lg_usr_agt,
                      pattern = "(bot|crawler|spider|checker|scanner|grabber|getter)[\\s_:,.;/)-]?")
                      # pattern = "(bot|spider)[\\s_:,.;/)-]")
   )
-# 
-# cz_bots.1 <- cz_stats.01 %>% 
-#   filter(str_detect(lg_usr_agt, pattern = "^(curl|wget)"))
-# 
-# cz_bots.2 <- cz_stats.01 %>% 
-#   filter(str_detect(lg_usr_agt,
-#                     pattern = "(bot|crawler|spider|checker|scanner|grabber|getter)[\\s_:,.;/)-]")
-#          )
-
-# collect user-agents
-# stats_ua <-
-#   cz_stats.02 %>% group_by(lg_usr_agt) %>% 
-#   summarise(n_visits = n(),
-#             streamed_MB = round(sum(lg_n_bytes)/1024/1024, digits = 2))
 
 # reproduce csv-example
 cz_stats.03 <- cz_stats.02 %>% 
   mutate(lg_date = date(lg_cz_ts)) %>% 
-  filter(lg_session_length >= 15L)
+  filter(lg_listened_secs > 30L)
 
 cz_stats.04 <- cz_stats.03 %>% 
   group_by(lg_date) %>% 
   summarise(n_valid_sessions = n())
 
 write_delim(cz_stats.04, delim = "\t", file = "cz_stats_verify.tsv")
+
+cz_stats.05 <- cz_stats.03 %>% 
+  group_by(lg_date) %>% 
+  summarise(n_listened_hours = sum(lg_listened_secs) / 3600)
+
+write_delim(cz_stats.05, delim = "\t", file = "cz_stats_listened_hours.tsv")
+
+cz_stats.06 <- cz_stats.03 %>% 
+  filter(lg_cz_channel == "jazznotjazz") %>% 
+  group_by(lg_date) %>% 
+  summarise(n_listened_hours = sum(lg_listened_secs) / 3600)
+
+write_delim(cz_stats.06, delim = "\t", file = "cz_stats_listened_hours_jnj.tsv")
 
 write_delim(channels, delim = "\t", file = "cz_stats_verify_channels.tsv")
 
